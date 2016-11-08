4072d709f8e634407a89aa585b2664b219b96715
Joker <nice.way@live.com>
2016-11-08 16:01:34 +0300

    for issue #11

--- example/jade_extends/main.go
+++ example/jade_extends/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/Joker/jade"
 )
+
+var	funcMap = template.FuncMap{
+	"include": func(includePath string) (template.HTML, error) {
+		include_tpl, err := jade.ParseFile(includePath)
+		if err != nil {
+			log.Printf("\nParseFile error: %v", err)
+		}
+		log.Printf("%s\n\n", include_tpl)
+
+		go_partial_tpl, _ := template.New("partial").Parse(include_tpl)
+
+		buf := new(bytes.Buffer)
+		go_partial_tpl.Execute(buf, "")
+		return template.HTML(buf.String()), nil
+
+	},
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	layout, err := jade.ParseFile("layout.jade")
@@ -23,7 +41,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	//
 
-	go_tpl, err := template.New("layout").Parse(layout)
+	go_tpl, err := template.New("layout").Funcs(funcMap).Parse(layout)
 	go_tpl.New("index").Parse(index)
 	if err != nil {
 		log.Printf("\nTemplate parse error: %v", err)
